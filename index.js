4a520f97b7a30028a8cd2e13c039b666e139c5fb
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-06-13 13:31:55 +0530

    Displaying form

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 import {createServer} from "http";
 import guitars from './data.js';
+import { createList,getGuitarContent, view,getForm } from "./content.js";
 
 const server=createServer((request,response)=>{
-    response.writeHead(200,{"Content-type":"text/html ;charset=utf-8"});
-    const url=new URL(request.url,'http://localhost') //request.url only gives the "/?id=3" so we have provide the base url
-    //console.log(request.url);
-    const id=url.searchParams.get('id'); //extract the value at teh id in the query param
-    const content = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Guitars</title>
-    </head>
-    <body style="font-size: 2rem">
-        ${id ? getGuitarContent(id):createList()}
-    </body>
-    </html>`;
-    response.end(content);
+
+    const part=request.url.split("/"); //this will extarct the "/delete/id=1" part from the url "http://localhost/delete/id=1"
+    if (part.includes("delete")) {
+        handleDelete(part[2])  //from the url piece "../delete/id"   .. is part[0], delete is part[1], id is part[2]
+        redirect(response,"/");
+    }else{
+        response.writeHead(200,{"Content-type":"text/html ;charset=utf-8"});
+        const url=new URL(request.url,'http://localhost') //request.url only gives the "/?id=3" so we have provide the base url
+        //console.log(request.url);
+        const id=url.searchParams.get('id'); //extract the value at the id in the query param
+        let content="";
+        if (part.includes("add")) {
+            content=getForm();
+        }else if(id){
+            let guitar=guitars.find((g)=>{return g.id==id}) //find in each object id is equal to the provided id if equal returns true
+            content=getGuitarContent(guitar);
+        }else{
+            content=createList(guitars);
+        }
+
+        
+        response.end(view(content));
+    }
 });
 
-const createList=()=>
-    `<h2>My Guitars</h2>
-    <ul>
-        ${guitars.map(createListItem).join('\n')}
-    </ul>`
-    ;
+function handleDelete(id){
+    const index=guitars.findIndex(g=>g.id==id);
+    guitars.splice(index,1);
+}
 
-    const createListItem = ({id, make, model}) => `<li><a href="?id=${id}">${make} ${model}</a></li>`;
-
-const getGuitarContent=(id)=>{
-    const guitar=guitars.find((g)=>{return g.id==id}) //find in each object id is equal to the provided id if equal returns true
-    if (guitar) {
-        return `<h2>${guitar.make} ${guitar.model}</h2>`;
-    }else{
-        return `<h2>guitar doesnt exist</h2>`;
-    }
+function redirect(response,to) {
+    response.writeHead(302,{location:to,"content-type":"text/plain"});
+    response.end(`redirect to ${to}`);
 }
 server.listen(80,()=>{
     console.log(`server is running at the port ${server.address().port}` )
