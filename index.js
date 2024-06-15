8c4d4248ff73173cf7005cce882ec9e711a25a91
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-06-15 23:57:46 +0530

    processing form data

--- index.js
+++ index.js
@@ -5,26 +5,45 @@ import { createList,getGuitarContent, view,getForm } from "./content.js";
 const server=createServer((request,response)=>{
 
     const part=request.url.split("/"); //this will extarct the "/delete/id=1" part from the url "http://localhost/delete/id=1"
-    if (part.includes("delete")) {
-        handleDelete(part[2])  //from the url piece "../delete/id"   .. is part[0], delete is part[1], id is part[2]
-        redirect(response,"/");
-    }else{
-        response.writeHead(200,{"Content-type":"text/html ;charset=utf-8"});
-        const url=new URL(request.url,'http://localhost') //request.url only gives the "/?id=3" so we have provide the base url
-        //console.log(request.url);
-        const id=url.searchParams.get('id'); //extract the value at the id in the query param
-        let content="";
-        if (part.includes("add")) {
-            content=getForm();
-        }else if(id){
-            let guitar=guitars.find((g)=>{return g.id==id}) //find in each object id is equal to the provided id if equal returns true
-            content=getGuitarContent(guitar);
-        }else{
-            content=createList(guitars);
-        }
+    
+    if (request.method =="POST") {
+        let body="";
 
-        
-        response.end(view(content));
+        request.on("readable", ()=>{
+            const data=request.read();//read() method checks whether the data comming as request is readble or net 
+            
+            if (data!= null) {
+                body+=data;
+            }
+
+        });
+
+        request.on("end",()=>{
+            console.log(body);
+        });
+    }else{
+    
+        if (part.includes("delete")) {
+            handleDelete(part[2])  //from the url piece "../delete/id"   .. is part[0], delete is part[1], id is part[2]
+            redirect(response,"/");
+        }else{
+            response.writeHead(200,{"Content-type":"text/html ;charset=utf-8"});
+            const url=new URL(request.url,'http://localhost') //request.url only gives the "/?id=3" so we have provide the base url
+            //console.log(request.url);
+            const id=url.searchParams.get('id'); //extract the value at the id in the query param
+            let content="";
+            if (part.includes("add")) {
+                content=getForm();
+            }else if(id){
+                let guitar=guitars.find((g)=>{return g.id==id}) //find in each object id is equal to the provided id if equal returns true
+                content=getGuitarContent(guitar);
+            }else{
+                content=createList(guitars);
+            }
+
+            
+            response.end(view(content));
+        }
     }
 });
 
