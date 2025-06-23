02078d36654d8e5d261d0823a33787867ce5f11e
DhananjayaDilz <158190487+DhananjayaDilz@users.noreply.github.com>
2025-06-23 23:59:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 // Middleware to parse  the JSON
 app.use(express.json());
 
-// Simple POST endpoint
+//A Simple POST endpoint
 app.post('/test-json', (req, res) => {
   console.log('Received JSON:', req.body);
   res.status(200).json({
