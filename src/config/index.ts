0c5d510d355e6324fbd529423f0abcfe2561a3c2
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2025-07-24 19:10:53 +0530

    setup rate limit, cors options

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,8 @@ dotenv.config();
 const config ={
     PORT : process.env.PORT || 3000,
     NODE_ENV : process.env.NODE_ENV,
-    WHITELIST_ORIGINS : ["http://localhost:3000"]
+    WHITELIST_ORIGINS : ["http://localhost:3000"],
+    MONGO_URI : process.env.MONGO_URI
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,30 @@
  */
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import helmet from "helmet";
 
 
 /*
  * Custom modules
  */
 import config from "@/config";
+import limiter from "@/lib/express_rate_limit";
+import { connectToDatabase ,disconnectFromDatabase } from "@/lib/mongoose";
 
 
 /**
  * Types
  */
 import type { CorsOptions } from "cors";
+
+
+/**
+ * Routes
+ */
+import v1Routes from "@/routes/v1"
+
 
 
 const app = express();
@@ -41,14 +53,57 @@ const corsOptions : CorsOptions = {
 };
 
 //Apply cors middleware
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.get("/", (req,res) =>{
-    res.json({
-        message: "hello typescript"
-    })
-})
+app.use(cookieParser());
 
-app.listen(config.PORT, ()=>{
-    console.log(`server running : http://localhost:${config.PORT}`)
-});
+app.use(compression({
+    threshold:1024 
+}));
+
+app.use(helmet());
+
+app.use(limiter);
+
+
+
+(async ()=>{
+    try {
+        await connectToDatabase();
+        app.use("/api/v1", v1Routes);
+        
+        app.listen(config.PORT, ()=>{
+            console.log(`server running : http://localhost:${config.PORT}`)
+        });
+    } catch (err) {
+        console.log("fail to sartt server" ,err)
+
+        if (config.NODE_ENV== "production") {
+            process.exit(1)
+        }
+    }
+})();
+
+
+/* Handles server shutdown gracefully by disconnecting from the database.
+* - Attempts to disconnect from the database before shutting down the
+server.
+*-Logs a success message if the disconnection is successful.
+* - If an error occurs during disconnection, it is logged to the console.
+* - Exits the process with status code '0' (indicating a successful shutdown).
+*/
+const handleServerShutdown = async () =>{
+    try {
+        await disconnectFromDatabase();
+        console.log("server SHUTDOWN");
+        process.exit(0)
+    } catch (err) {
+        console.log("Error during server shutting down")
+    }
+};
+
+
+//SIGTERM Sent when user kill the process
+// SIGINT Sent when user interupt the process by pressing ctrl + c  both of these hapen the function handleServerShutdown executes
+process.on("SIGTERM", handleServerShutdown);  
+process.on("SIGINT", handleServerShutdown);   
